fba41202ec9045248efd5f07367b79bbcb9d7370
hedlundemma <112821036+hedlundemma@users.noreply.github.com>
2023-04-21 14:31:53 +0200

    buttons letters

--- animal-hangman/src/components/Letters/index.js
+++ animal-hangman/src/components/Letters/index.js
@@ -1,11 +1,25 @@
 import styled from 'styled-components';
 
 
-export const StyldedLetterButton = styled.button`
-width: 50px;
-  height: 20px;
+ const StyledLetterButton = styled.button`
+  width: 50px;
+  height: 50px;
   border-radius: 20px;
+  background-color: yellow;
+  color: black;
+  cursor: pointer;
 `
+
+const LetterButton = props => {
+    return (
+    
+    <StyledLetterButton className="btn">{props.buttonText}</StyledLetterButton>
+  
+    )
+
+}
+
+export default LetterButton;
 
 
 
