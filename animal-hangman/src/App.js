2ba159977f83c5035085818dac1be8b73c4d5905
jaken92 <jaaakeeen@gmail.com>
2023-04-25 19:18:06 +0200

    deleted old components

--- animal-hangman/src/App.js
+++ animal-hangman/src/App.js
@@ -1,7 +1,6 @@
 import { StyledButton } from './components/StartButton/Button';
 import Figure from './components/Figure';
 import Podium from './components/Podium';
-import { WordGenerator } from './components/WordGenerator/WordGenerator';
 import { Word } from './components/Word/Word';
 
 import LetterButton from './components/Letters';
@@ -17,9 +16,11 @@ function RedirectButton(props) {
   };
   return <button onClick={handleClick}>Go down on page</button>;
 }
+
 let word = 'Word';
 let category = 'pending';
 let letters = 0;
+
 function App() {
   async function handleClick() {
     let response = await fetch(
@@ -42,8 +43,6 @@ function App() {
   console.log(theWord.length);
 
   return (
-
-    
     <div className="App">
       <Word
         Category={theCategory}
@@ -56,37 +55,36 @@ function App() {
       {/* <Podium></Podium>
 
       <LetterButton></LetterButton> */}
-
+      <h1>{theWord}</h1>
       <RedirectButton divId="letter-div"></RedirectButton>
       <Podium></Podium>
-      <WordContainer></WordContainer>
-      <div className = "LetterContainer">
-      <LetterButton value = "a" buttonText = "A"></LetterButton>
-      <LetterButton buttonText = "B"></LetterButton>
-      <LetterButton buttonText = "C"></LetterButton>
-      <LetterButton buttonText = "D"></LetterButton>
-      <LetterButton buttonText = "E"></LetterButton>
-      <LetterButton buttonText = "F"></LetterButton>
-      <LetterButton buttonText = "G"></LetterButton>
-      <LetterButton buttonText = "H"></LetterButton>
-      <LetterButton buttonText = "I"></LetterButton>
-      <LetterButton buttonText = "J"></LetterButton>
-      <LetterButton buttonText = "K"></LetterButton>
-      <LetterButton buttonText = "L"></LetterButton>
-      <LetterButton buttonText = "M"></LetterButton>
-      <LetterButton buttonText = "N"></LetterButton>
-      <LetterButton buttonText = "O"></LetterButton>
-      <LetterButton buttonText = "P"></LetterButton>
-      <LetterButton buttonText = "Q"></LetterButton>
-      <LetterButton buttonText = "R"></LetterButton>
-      <LetterButton buttonText = "S"></LetterButton>
-      <LetterButton buttonText = "T"></LetterButton>
-      <LetterButton buttonText = "U"></LetterButton>
-      <LetterButton buttonText = "V"></LetterButton>
-      <LetterButton buttonText = "W"></LetterButton>
-      <LetterButton buttonText = "X"></LetterButton>
-      <LetterButton buttonText = "Y"></LetterButton>
-      <LetterButton buttonText = "Z"></LetterButton>
+      <div className="LetterContainer">
+        <LetterButton value="a" buttonText="A"></LetterButton>
+        <LetterButton buttonText="B"></LetterButton>
+        <LetterButton buttonText="C"></LetterButton>
+        <LetterButton buttonText="D"></LetterButton>
+        <LetterButton buttonText="E"></LetterButton>
+        <LetterButton buttonText="F"></LetterButton>
+        <LetterButton buttonText="G"></LetterButton>
+        <LetterButton buttonText="H"></LetterButton>
+        <LetterButton buttonText="I"></LetterButton>
+        <LetterButton buttonText="J"></LetterButton>
+        <LetterButton buttonText="K"></LetterButton>
+        <LetterButton buttonText="L"></LetterButton>
+        <LetterButton buttonText="M"></LetterButton>
+        <LetterButton buttonText="N"></LetterButton>
+        <LetterButton buttonText="O"></LetterButton>
+        <LetterButton buttonText="P"></LetterButton>
+        <LetterButton buttonText="Q"></LetterButton>
+        <LetterButton buttonText="R"></LetterButton>
+        <LetterButton buttonText="S"></LetterButton>
+        <LetterButton buttonText="T"></LetterButton>
+        <LetterButton buttonText="U"></LetterButton>
+        <LetterButton buttonText="V"></LetterButton>
+        <LetterButton buttonText="W"></LetterButton>
+        <LetterButton buttonText="X"></LetterButton>
+        <LetterButton buttonText="Y"></LetterButton>
+        <LetterButton buttonText="Z"></LetterButton>
       </div>
     </div>
   );
